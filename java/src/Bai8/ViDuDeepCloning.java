393ed51f3c45e8844813e14971589df9d0010902
phuhoag <147980728+phuhoag@users.noreply.github.com>
2024-06-10 09:39:05 +0700

    ViDuDeepCloning.java

--- java/src/Bai8/ViDuDeepCloning.java
+++ java/src/Bai8/ViDuDeepCloning.java
@@ -1,12 +1,31 @@
 package Bai8;
 
+import java.util.ArrayList;
 
+import bai08.vidu4.HoaDon;
+import bai08.vidu4.SanPham;
 public class ViDuDeepCloning {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		ArrayList<SanPham> dssp = new ArrayList<SanPham>();
+		dssp.add(new SanPham(1, "Nokia"));
+		dssp.add(new SanPham(2, "SamSung"));
+		HoaDon hd1 = new HoaDon(0, dssp);
+		System.out.println("Hoa don 1: " + hd1);
+		HoaDon hd2;
+		try {
+			hd2 = (HoaDon)hd1.clone();
+			hd2.setMaHoaDon(15);
+			hd2.getDssp().getFirst().setMaSanPham(18);
+			hd2.getDssp().getFirst().setTenSanPham("IPhone");
+			System.out.println("Hoa don 1: " + hd1);
+			System.out.println("Hoa don 2: " + hd2);
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
